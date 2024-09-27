a807a1e7880cc2b05525317c3d10ae542d02f8e7
nitai-b <nitaibarran5@gmail.com>
2024-09-27 19:13:53 -0400

    got player triangle showing up on the game

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
-    game = pygame.init()
-    print("Starting asteroids!")
-    print("Screen width: " + str(SCREEN_WIDTH))
-    print("Screen height: " + str(SCREEN_HEIGHT))
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        screen.fill("black")
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        res = clock.tick(60)
-        dt = res / 1000
-        print(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        res = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
