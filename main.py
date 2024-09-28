bf3a4733912bfa184322463298e4128560f71a72
nitai-b <nitaibarran5@gmail.com>
2024-09-28 12:30:48 -0400

    player can rotate with these changes to the player class

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
-        res = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
